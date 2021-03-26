a64bc016bd9c9c8b85c00250056d501e1395d70e
peterman.dd@gmail.com <peterman.dd@gmail.com>
2021-03-25 23:10:55 -0400

    created skeleton of widgets examples page

--- ST_Widget_Examples_Page/widgets_examples_page.js
+++ ST_Widget_Examples_Page/widgets_examples_page.js
@@ -1,6 +1,6 @@
 
 function scroll_to_details(demo) {
     let demoId = demo.getAttribute('id');
-    let demoDetails = document.getElementById(`${demoId}-detailed`);
+    let demoDetails = document.getElementById(`${demoId}-details`);
     demoDetails.scrollIntoView({behavior: 'smooth'});
 }
