8ddfbde397caa2f19c7bca2e04fefb5a78d67f66
peterman.dd@gmail.com <peterman.dd@gmail.com>
2021-03-31 12:04:44 -0400

    Initial commit

--- ST_Widget_Examples_Page/widgets_examples_page.js
+++
@@ -1,6 +0,0 @@
-
-function scroll_to_details(demo) {
-    let demoId = demo.getAttribute('id');
-    let demoDetails = document.getElementById(`${demoId}-details`);
-    demoDetails.scrollIntoView({behavior: 'smooth'});
-}
